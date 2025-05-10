cf9621ff1e216e3e7206435d6ed5561bd03b9eb9
shaikhjavedofficial <skjaved633@gmail.com>
2025-05-10 23:05:58 +0530

    [feature]-added redis cloud connection

--- redisClient.js
+++ redisClient.js
@@ -1,12 +1,13 @@
-// Redis caching utility
 const redis = require("redis");
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
-  legacyMode: true, // for Node Redis v4 compatibility with callbacks
+  username: process.env.REDIS_USERNAME || "default",
+  password: process.env.REDIS_PASSWORD || "",
+  socket: {
+    host: process.env.REDIS_HOST || "redis://localhost",
+    port: process.env.REDIS_PORT || 6379,
+  },
+  legacyMode: true,
 });
 redisClient.connect().catch(console.error);
-
-// No need to promisify, use built-in Promise API in redis v4+
-
 module.exports = redisClient;
